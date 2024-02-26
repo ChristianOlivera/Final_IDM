b6b16bdc9ef61992b42b006fdbf205d120a2fb22
ChristianOlivera <christian.olivera@davinci.edu.ar>
2024-02-25 21:58:33 -0300

    css animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker.register('/sw.js')
     .then(registration => {
-    console.log('Service Worker registrado con éxito:', registration);
+    console.log('Service Worker registrado con éxito:', registration)
     })
     .catch(error => {
-    console.log('Error al registrar el Service Worker:', error);
-    });
+    console.log('Error al registrar el Service Worker:', error)
+    })
 }
 document.getElementById('descargar_app').addEventListener('click', instalarPWA)
 
@@ -18,4 +18,25 @@ function instalarPWA() {
 
 window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elementosAnimados = document.querySelectorAll('.navegacion_imagenes > img, .compra_imagenes > img');
+  
+    const opciones = {
+      threshold: 0.8
+    }
+
+    const animacion = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animada')
+            observer.unobserve(entry.target)
+          }
+        })
+      }, opciones)
+    
+      elementosAnimados.forEach(function(elemento) {
+        animacion.observe(elemento)
+      })
 })
